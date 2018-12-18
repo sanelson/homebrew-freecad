73abc1ed83dc18dd5c88a9eb121d60feae9bf53f
Bruce B. Lacey <bruce@blacey.com>
2018-12-18 14:41:18 -0500

    qtwebkit: brew test-bot compliance

--- Formula/qtwebkit.rb
+++ Formula/qtwebkit.rb
@@ -1,9 +1,8 @@
 class Qtwebkit < Formula
   desc "Qt Webkit"
   homepage "https://wiki.qt.io/Qt_WebKit"
-  url "https://code.qt.io/qt/qtwebkit.git", :using => :git, :branch => '5.212',
-    :revisoin => '72cfbd7664f21fcc0e62b869a6b01bf73eb5e7da'
-  head "https://code.qt.io/qt/qtwebkit.git", :using => :git, :branch => '5.212'
+  url "https://code.qt.io/qt/qtwebkit.git", :branch => "5.212", :revisoin => "72cfbd7664f21fcc0e62b869a6b01bf73eb5e7da"
+  head "https://code.qt.io/qt/qtwebkit.git", :branch => "5.212"
   version "5.212-72cfbd"
   revision 3
 
@@ -20,11 +19,10 @@ class Qtwebkit < Formula
   depends_on "libpng"
   depends_on "cmake" => :build
   depends_on "ninja" => :build
-  
-  keg_only "Qt itself is keg only which implies the same for Qt modules"
-  
+
+  keg_only "qt itself is keg only which implies the same for qt modules"
+
   def install
     system "./Tools/Scripts/build-webkit", "--qt", "--prefix=#{prefix}", "--install"
   end
 end
-
