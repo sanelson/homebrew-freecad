b599c6cff0e2c58eb5db66842eb71ce944df4986
Bruce B. Lacey <bruce@blacey.com>
2018-12-29 16:47:25 -0500

    qtwebkit: revision 3 obviated by upgrade to 5.212

--- Formula/qtwebkit.rb
+++ Formula/qtwebkit.rb
@@ -3,7 +3,6 @@ class Qtwebkit < Formula
   homepage "https://wiki.qt.io/Qt_WebKit"
   url "https://code.qt.io/qt/qtwebkit.git", :branch => "5.212", :revision => "72cfbd7664f21fcc0e62b869a6b01bf73eb5e7da"
   version "5.212-72cfbd"
-  revision 3
   head "https://code.qt.io/qt/qtwebkit.git", :branch => "5.212"
 
   keg_only "qt itself is keg only which implies the same for qt modules"
